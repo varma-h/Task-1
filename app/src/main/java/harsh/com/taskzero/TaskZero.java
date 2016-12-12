8f4df35eb5ec98a8ea1659144f1e24f09c804722
Harsh Varma <Harsh Varma>
2016-12-12 13:53:10 +0530

    Demo.txt file created

---
+++ app/src/main/java/harsh/com/taskzero/TaskZero.java
@@ -0,0 +1,13 @@
+package harsh.com.taskzero;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaskZero extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_zero);
+    }
+}
